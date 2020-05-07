81acf023e12bdb6454594619f41e060be7fee095
Vamshi Vangapally <Vamshi.Vangapally@ey.com>
2020-05-07 13:28:45 -0400

    adding ajax call in profile to fetch github profil

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './profile.css'
 
-function App() {
-  return (
+function Profile(props) {
+  const initialUserState = {
+    user:{},
+    loading: true
+  }
+
+  const [user, setUser] = useState(initialUserState);
+  useEffect(()=>{
+    const getUser = async() => {
+      const { data } = await axios("https://api.github.com/users/vamshi4001");
+      setUser(data);
+    }
+
+    getUser();
+  }, [])
+  return user.loading?
+  (
+    <div>Loading...</div>
+  ):(
     <div className="container-fluid">
-      <h1 className="mt-4">Profile</h1>
-      <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-      <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+      <h1 className="mt-4">Github Profile of vamshi4001</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Website</th>
+            <th>Followers</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.location}</td>
+            <td><a href={user.blog}>{user.blog}</a></td>
+            <td>{user.followers}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default Profile;
