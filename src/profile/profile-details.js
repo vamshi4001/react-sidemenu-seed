579b6a5f6baf0577116033ad3dd4f3a5172299aa
Vamshi Vangapally <Vamshi.Vangapally@ey.com>
2020-05-12 16:36:16 -0400

    redux intro and ajax calls on dispatch

--- src/profile/profile-details.js
+++ src/profile/profile-details.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './profile.css'
+import {Post} from './post';
+import {connect} from 'react-redux';
+import { fetchPosts } from './actions/postActions';
+
+// import './profile.css'
 
 function ProfileDetails(props) {
   const initialUserState = {
@@ -9,7 +13,9 @@ function ProfileDetails(props) {
   }
 
   const [user, setUser] = useState(initialUserState);
+  const {dispatch, loading, hasErrors, posts} = props;
   useEffect(()=>{
+    // dispatch(fetchPosts())
     const getUser = async() => {
       const { data } = await axios(`https://api.github.com/users/${props.match.params.id}`);
       setUser(data);
@@ -17,38 +23,75 @@ function ProfileDetails(props) {
 
     getUser();
   }, [props.match.params.id])
+
+  useEffect(()=>{
+    dispatch(fetchPosts())    
+  }, [dispatch])
+
+
+  const renderPosts = () =>{
+    if(loading) return (<p>Loading posts</p>)    
+    if(hasErrors) return (<p>Failed to load posts</p>)
+    return posts.map(post => <Post key={post.id} post={post}></Post>)
+  }
   return (
-    <div className="mt-4">
-      <button className="button ml-4">Back</button>
-      {user.loading?
-      (
-        <div>Loading...</div>
-      ):(
-        <div className="container-fluid">
-          <h1 className="mt-4">Github Profile of {props.match.params.id}</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Location</th>
-                <th>Website</th>
-                <th>Followers</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{user.name}</td>
-                <td>{user.location}</td>
-                <td><a href={user.blog}>{user.blog}</a></td>
-                <td>{user.followers}</td>
-              </tr>
-            </tbody>
-          </table>
+    <div className="m-4">
+      {/* <button className="button ml-4">Back</button> */}
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <a className="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Github</a>
+        </li>
+        <li className="nav-item" role="presentation">
+          <a className="nav-link" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Posts</a>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div className="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <section>
+          <h2>Posts</h2>
+          {renderPosts()}
+        </section>
         </div>
-      )}
+        <div className="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+          {user.loading?
+          (
+            <div>Loading...</div>
+          ):(
+            <div className="container-fluid">
+              <h1 className="mt-4">Github Profile of {props.match.params.id}</h1>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Website</th>
+                    <th>Followers</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{user.name}</td>
+                    <td>{user.location}</td>
+                    <td><a href={user.blog}>{user.blog}</a></td>
+                    <td>{user.followers}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>            
     </div>
   )
-  
 }
 
-export default ProfileDetails;
+const mapStateToProps = function(state){
+  return{
+    loading: state.posts.loading,
+    hasErrors: state.posts.hasErrors,
+    posts: state.posts.posts,
+    props: state.props
+  }
+}
+
+export default connect(mapStateToProps)(ProfileDetails);
--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import {Route, Link } from 'react-router-dom';
 import ProfileDetails from './profile-details'
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './profile.css'
+
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 function Profile() {
   return (
-    <div className="mt-4">
-      <ul>
-        <li><Link to="/profile/vamshi4001">Vamshi4001</Link></li>
-        <li><Link to="/profile/pradyumnad">prady</Link></li>
-        <li><Link to="/profile/srameshr">Sid</Link></li>
-      </ul>
+    <Provider store={store}>
       <div className="mt-4">
-        <Route path="/profile/:id" component={ProfileDetails} />
+        <ul>
+          <li><Link to="/profile/vamshi4001">Vamshi4001</Link></li>
+          <li><Link to="/profile/pradyumnad">prady</Link></li>
+          <li><Link to="/profile/srameshr">Sid</Link></li>
+        </ul>
+        <div className="mt-4">
+          <Route path="/profile/:id" component={ProfileDetails} />
+        </div>
       </div>
-    </div>
+    </Provider>
   )
   
 }
